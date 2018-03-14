2f49f82fea1a36d1eb52584d0e1b0365ef903d44
trolleksii <trolleksii@gmail.com>
2018-03-14 16:02:10 +0200

    made app urls and index.html

--- timestamp_project/timestamp_app/urls.py
+++ timestamp_project/timestamp_app/urls.py
@@ -1,6 +1,10 @@
 from django.urls import path
 
+from .views import IndexView, TimeStampView
+
 app_name = 'timestamp_app'
 
 urlpatterns = [
+    path('', IndexView.as_view(), name='index_view'),
+    path('<str:date>/', TimeStampView.as_view(), name='timestamp_view'),
 ]
