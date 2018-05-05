b2f90ee5b89767441f4c13cea67e4bf5ccd64f6d
trolleksii <trolleksii@gmail.com>
2018-05-05 11:52:40 +0300

    added url parameters to display site url on the index page

--- timestamp_project/timestamp_app/urls.py
+++ timestamp_project/timestamp_app/urls.py
@@ -1,10 +1,12 @@
 from django.urls import path
 
+from timestamp_project.settings import SITE_URL
+
 from .views import IndexView, TimeStampView
 
 app_name = 'timestamp_app'
 
 urlpatterns = [
-    path('', IndexView.as_view(), name='index_view'),
+    path('', IndexView.as_view(), {'site_url': SITE_URL}, name='index_view'),
     path('<str:date>/', TimeStampView.as_view(), name='timestamp_view'),
 ]
